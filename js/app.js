9de141706c32b499fe2c70040700ee7a16a17c69
kateladenheim <ladenheim.k@gmail.com>
2016-08-14 18:36:08 -0400

    commit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
 $(document).ready(function () {
 	
-	// console.logs the window.width
 	$(window).resize(function () {
 		console.log($(window).width());
 		if ($(window).width() >= 1024) {
 			$('#mobile-nav').hide(); 
 		}
+
+	});
+
+	$(window).resize(function () {	
+		if ($(window).width() >= 850) {
+			$('mobile-h1').hide();
+		}
 	});
 
 $('#burger').click(function () {
 	$('#mobile-nav').slideToggle();
+});
+
+
+$(#'desktop-h1').load(function () {
+	$('#desktop-h1').addClass('fadeInUp');
 });
 	
 });
